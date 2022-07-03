2a0f4020ec3661d035a3948e2faab67207eb8ad4
romildojuliano <rjal@cin.ufpe.br>
2022-07-03 11:38:49 -0300

    Data input simplified

--- arb.py
+++ arb.py
@@ -1,3 +1,7 @@
+from platform import platform
+import pandas as pd
+import numpy as np
+
 class Arbitrage:
     odd_a: float
     odd_b: float
@@ -8,36 +12,87 @@ class Arbitrage:
     bet_a: float
     bet_b: float
     bet_draw: float
+    platform: str
 
-    def __init__(self, odd_a: float, odd_b: float, odd_draw: float) -> None:
+    def __init__(self, odd_a: float, odd_b: float, odd_draw: float, platform: str, total_bet: float = np.NaN) -> None:
         self.odd_a = odd_a
         self.odd_b = odd_b
         self.odd_draw = odd_draw
+        self.total_bet = total_bet
+        self.platform = platform
         self.tip = (1 / self.odd_a + 1 / self.odd_b + 1 / self.odd_draw)
         self.profit = (1 - self.tip)
-        print(f'total implied probability: {self.tip*100:.2f}%')
+        print(f'Total implied probability: {self.tip*100:.2f}%')
 
-    def calculate_bets(self) -> None:
-        if self.profit > 0:
-            print('----------------------------------')
+    def calculate_bets(self, verbose: bool = False, user_input: bool = False) -> pd.DataFrame:
+        """Insert docstring here
+
+        Args:
+            verbose (bool, optional): Prints more details for visualization. 
+            Defaults to True.
+
+        Returns:
+            pd.DataFrame: Dataframe containing the values of the atual bet
+        """
+        if user_input == True:
             self.total_bet = float(input('total bet: '))
-            print(f'profit margin: {self.profit*100:.2f}%')
-            print(f'expected profit: R$ {self.total_bet*self.profit:.2f}')
-            self.bet_a = (self.total_bet * (1 / self.odd_a)) / self.tip
-            self.bet_b = (self.total_bet * (1 / self.odd_b)) / self.tip
-            self.bet_draw = (self.total_bet * (1 / self.odd_draw)) / self.tip
-            print('----------------------------------')
-            print(f'bet on outcome A: R$ {self.bet_a:.2f}')
-            print(f'bet on outcome B: R$ {self.bet_b:.2f}')
-            print(f'bet on draw: R$ {self.bet_draw:.2f}')
-        else:
-            print(
-                "Can't guarantee profit with total implied probability greater than 100%")
 
+        
+
+        self.bet_a = (self.total_bet * (1 / self.odd_a)) / self.tip
+        self.bet_b = (self.total_bet * (1 / self.odd_b)) / self.tip
+        self.bet_draw = (self.total_bet * (1 / self.odd_draw)) / self.tip
+
+        if verbose == True:
+            print('----------------------------------')
+            print(f'Profit margin: {self.profit*100:.2f}%')
+            print(f'Expected profit: R$ {self.total_bet*self.profit:.2f}')
+            print('----------------------------------')
+            print(f'Bet on outcome A: R$ {self.bet_a:.2f}')
+            print(f'Bet on outcome B: R$ {self.bet_b:.2f}')
+            print(f'Bet on draw: R$ {self.bet_draw:.2f}')
+
+            if self.profit < 0:
+                print(
+                    "Can't guarantee profit with total implied probability \
+                    greater than 100%"
+                )
+        
+        # Returns a pandas dataframe that can be used to store some bets
+        # if you plan on storing multiple values, you may want to append
+        # these values on some kind of main dataframe.
+        return pd.DataFrame(
+            data={
+                "Odd a":            [self.odd_a],
+                "Odd Draw":         [self.odd_draw],
+                "Odd b":            [self.odd_b],
+                "Bet a":            [self.bet_a],
+                "Draw" :            [self.bet_draw],
+                "Bet b":            [self.bet_b],
+                "Profit Rate":      [self.profit],
+                "Expected Profit":  [self.profit*self.total_bet]
+            },
+            index=[self.platform]
+        )
 
 if __name__ == '__main__':
+    # Using user input
+    '''
     odd_a = float(input('odd for outcome A: '))
     odd_b = float(input('odd for outcome B: '))
     odd_draw = float(input('odd for draw: '))
     arb = Arbitrage(odd_a, odd_b, odd_draw)
-    arb.calculate_bets()
+    df = arb.calculate_bets(verbose=True)
+    print(df)
+    '''
+    
+    # Using constructor itself
+    generic_bet = Arbitrage(
+        odd_a=1.34, 
+        odd_b=8.86, 
+        odd_draw=5.29, 
+        total_bet=2500,
+        platform="BETSY"
+    ).calculate_bets()
+    
+    print(generic_bet)
